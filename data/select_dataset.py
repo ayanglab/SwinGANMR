2fcc1cf1cb26c576039f408c40b7cb956658542b
Jiahao Huang <55026816+JiahaoHuang99@users.noreply.github.com>
2022-09-05 21:46:18 +0100

    Update dataset

--- data/select_dataset.py
+++ data/select_dataset.py
@@ -1,26 +1,20 @@
-
+'''
+# -----------------------------------------
+Select Dataset
+by Jiahao Huang (j.huang21@imperial.ac.uk)
+# -----------------------------------------
+'''
 
 
 def define_Dataset(dataset_opt):
     dataset_type = dataset_opt['dataset_type'].lower()
 
-    if dataset_type in ['miccai']:
-        from data.dataset_MICCAI import DatasetMICCAI as D
-
-    elif dataset_type in ['cc']:
-        from data.dataset_CC import DatasetCC as D
-
-    elif dataset_type in ['ccpi']:
-        from data.dataset_CCpi import DatasetCCpi as D
-
-    elif dataset_type in ['ccaxi']:
-        from data.dataset_CCaxi import DatasetCCaxi as D
-
-    elif dataset_type in ['fastmri']:
-        from data.dataset_FastMRI import DatasetFastMRI as D
-
-    elif dataset_type in ['brats17']:
-        from data.dataset_BraTS17 import DatasetBraTS17 as D
+    # ------------------------------------------------
+    # CC-359 Calgary Campinas Public Brain MR Dataset
+    # ------------------------------------------------
+    # CC-SAG-NPI d.1.2
+    if dataset_type in ['ccsagnpi']:
+        from data.dataset_CCsagnpi import DatasetCCsagnpi as D
 
     else:
         raise NotImplementedError('Dataset [{:s}] is not found.'.format(dataset_type))
